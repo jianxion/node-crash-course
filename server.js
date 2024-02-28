1f06ef48ab1d08928db8db35f403d90568e92ad2
jianxion <sjxshen@ucdavis.edu>
2024-02-28 15:02:35 -0500

    added notes on server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
@@ -8,53 +8,60 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+  // //res.write('<head><link rel = "stylesheet" href = "#"></head>'); // links an external stylesheet to the HTML document
   // res.write('<p>hello, ninjas</p>');
   // res.write('<p>hello again, ninjas</p>');
-  // res.end();
+  // res.end();  //})
 
   // send html file
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   //res.write(data);
-  //   res.end(data);
-  // });
-
-  // routing
-  let path = './views/';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
-      break;
-    case '/about':
-      path += 'about.html';
-      res.statusCode = 200;
-      break;
-    case '/about-us':
-      res.statusCode = 301;
-      res.setHeader('Location', '/about');
-      res.end();
-      break;
-    default:
-      path += '404.html';
-      res.statusCode = 404;
-  }
-
-  // send html
-  fs.readFile(path, (err, data) => {
+  fs.readFile('./views/index.html', (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     }
-    //res.write(data);
+    // res.write(data);
     res.end(data);
   });
-
-
 });
+
+  // routing
+//   let path = './views/';
+//   switch(req.url) {
+//     case '/':
+//       path += 'index.html';
+//       res.statusCode = 200; 
+//       break;
+//     case '/about':
+//       path += 'about.html';
+//       res.statusCode = 200;
+//       break;
+//     case '/about-us':
+//       res.statusCode = 301;
+//       res.setHeader('Location', '/about');
+//       res.end();
+//       break;
+//     default:
+//       path += '404.html';
+//       res.statusCode = 404;
+//   }
+// status code:
+// 200: ok
+// 301: resource moved
+// 404: not found
+// 500: internal server error
+
+//   // send html
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end();
+//     }
+//     //res.write(data);
+//     res.end(data);
+//   });
+
+
+// });
 
 // localhost is the default value for 2nd argument
 server.listen(3000, 'localhost', () => {
